b9117aafd8413a0f85d7284561a25383a5428870
abocardoq <43285890+abocardoq@users.noreply.github.com>
2025-06-15 21:58:01 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -15,7 +15,14 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
+
+if "GOOGLE_API_KEY" in st.secrets:
+    os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
+elif os.getenv("GOOGLE_API_KEY"):
+    os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
+else:
+    st.error("❌ No se encontró GOOGLE_API_KEY. Configúralo en .env o en Secrets.")
+    st.stop()
 
 # Inicializa el modelo de lenguaje
 llm = GoogleGenerativeAI(model="gemini-2.0-flash") # la API se configuro en las variables de entorno
